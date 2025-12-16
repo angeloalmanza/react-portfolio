ce23d5aa49dbd9299128c8d360b68e15621a34a9
angeloalmanza <angeloalmanza26@gmail.com>
2025-12-16 12:41:39 +0100

    About component

--- src/data/hero.js
+++ src/data/hero.js
@@ -1,44 +1,41 @@
 export const heroData = {
-    greeting: "👋 Hello I'm",
-    name: "Angelo",
-    roles: [
-        "Full Stack Developer",
-        "React Specialist",
-        "Creative Coder"
-    ],
-    description: "I’m a Full Stack Developer focused on building modern, scalable web applications, handling both frontend and backend with attention to performance and user experience.",
-    stats: [
-        {number: "2+", label: "Years Experience"},
-        {number: "10+", label: "Personal Project"},
-        {number: "300+", label: "Coding Hours"},
-    ],
-    socialLinks: [
-        {
-            name: "GitHub",
-            url: "https://github.com/angeloalmanza",
-            icon: "Github",
-        },
-        {
-            name: "Linkedin",
-            url: "https://www.linkedin.com/in/angelo-almanza/",
-            icon: "Linkedin",
-        },
-        {
-            name: "Email",
-            url: "mailto:angeloalmanza26@gmail.com",
-            icon: "Mail",
-        }
-    ],
-    ctaButtons: [
-        {
-            text: "View My Work",
-            href: "#project",
-            variant: "primary",
-        },
-        {
-            text: "Let's Connect",
-            href: "#Contact",
-            variant: "secondary",
-        }
-    ]
-}
+  greeting: "👋 Hello I'm",
+  name: "Angelo",
+  roles: ["Full Stack Developer", "React Specialist", "Creative Coder"],
+  description:
+    "I’m a Full Stack Developer focused on building modern, scalable web applications, handling both frontend and backend with attention to performance and user experience.",
+  stats: [
+    { number: "2+", label: "Years Experience" },
+    { number: "10+", label: "Personal Project" },
+    { number: "300+", label: "Coding Hours" },
+  ],
+  socialLinks: [
+    {
+      name: "GitHub",
+      url: "https://github.com/angeloalmanza",
+      icon: "Github",
+    },
+    {
+      name: "Linkedin",
+      url: "https://www.linkedin.com/in/angelo-almanza/",
+      icon: "Linkedin",
+    },
+    {
+      name: "Email",
+      url: "mailto:angeloalmanza26@gmail.com",
+      icon: "Mail",
+    },
+  ],
+  ctaButtons: [
+    {
+      text: "View My Work",
+      href: "#project",
+      variant: "primary",
+    },
+    {
+      text: "Let's Connect",
+      href: "#contact",
+      variant: "secondary",
+    },
+  ],
+};
