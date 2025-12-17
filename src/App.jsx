547deb57c10e09907b7be170fd63be89430e0c71
angeloalmanza <angeloalmanza26@gmail.com>
2025-12-17 01:43:03 +0100

    ScrollToTop component

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import ScrollToTop from "./components/ScrollToTop";
 
 const App = () => {
   const hasAnimated = useIntersectionObserver();
@@ -22,6 +23,7 @@ const App = () => {
       <Skills hasAnimated={hasAnimated}/>
       <Contact hasAnimated={hasAnimated}/>
       <Footer />
+      <ScrollToTop showScrollTop={showScrollTop}/>
     </div>
   );
 };
